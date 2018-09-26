055da8089a311bab4de448daea26ee12602ea34d
margauxCortier <cortier.margaux@gmail.com>
2018-09-26 15:12:43 +0200

    exo pyramide

---
+++ pyramide.rb
@@ -0,0 +1,10 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+print "> "
+num = gets.to_i
+fois = 1
+
+puts "voici la pyramide :"
+while fois <= num
+	puts ("#" * fois).rjust(num)
+	fois += 1
+end
